1c6edb4efce699670cd80392ad26e0fad82fe8ee
igorfcfs <igorfcfs@gmail.com>
2022-07-06 15:28:13 -0300

    Projeto Criado

---
+++ certificacao/src/certificacao/Teste.java
@@ -0,0 +1,13 @@
+package certificacao;
+
+public class Teste {
+
+	public void m1() {
+		System.out.println("Hello World!!!");
+	}
+	
+	public static void main(String[] args) {
+		Teste t = new Teste();
+		t.m1();
+	}
+}
